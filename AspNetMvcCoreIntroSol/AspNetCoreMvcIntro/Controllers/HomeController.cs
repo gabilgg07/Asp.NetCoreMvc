544deabd13e00041043b9bf01fcb225ad6259c82
gabilgg07 <gabilgg07@gmail.com>
2022-11-01 18:54:04 +0400

    Created new MVC project, Pattern, Routing

--- AspNetMvcCoreIntroSol/AspNetCoreMvcIntro/Controllers/HomeController.cs
+++ AspNetMvcCoreIntroSol/AspNetCoreMvcIntro/Controllers/HomeController.cs
@@ -1,37 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using AspNetCoreMvcIntro.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AspNetCoreMvcIntro.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult GetPerson()
         {
-            return View();
-        }
+            var obj = new
+            {
+                name = "Samir",
+                surname="Hemidov"
+            };
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Json(obj);
         }
     }
 }
